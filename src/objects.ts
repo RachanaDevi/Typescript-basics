8f25e55d1ac3a1ba0096de702c4e0390fa79b448
Rachana Devi <rachanadevi18@gmail.com>
2023-03-17 16:40:36 +0530

    Concept of readonly in Objects

--- src/objects.ts
+++ src/objects.ts
@@ -1,5 +1,8 @@
 let previousEmployee = { id: 1, name: 'Rachana' }
-let employee: { id: number, name: string } = { id: 1, name: ' Rachana' };
+let employee: { readonly id: number, name: string } = { id: 1, name: ' Rachana' };
+
+// we should not able to change this so we add readonly
+// employee.id = 2
 
 console.log(employee.name);
 
