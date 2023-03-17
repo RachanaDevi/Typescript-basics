d376375487527a69ffb8b825064d37fcce906e19
Rachana Devi <rachanadevi18@gmail.com>
2023-03-17 16:17:05 +0530

    Add function and variables and allow anyType check in tsconfig.json

--- src/primitive_dataypes.ts
+++ src/primitive_dataypes.ts
@@ -8,4 +8,16 @@ let other_sales = 1234_434;
 let other_course = 'Typescript course'
 let other_is_published = true;
 
+
+// ANY TYPE
+// this makes it lose the core of typescript which is typesafety so you should avoid using `any` type
 let anyVariable; // this is of type any
+anyVariable = 1;
+anyVariable = 'a'
+
+
+// you can update tsconfig.json to not be strict about any
+// function render(document: any) {
+function render(document) {
+    console.log(document);
+}
