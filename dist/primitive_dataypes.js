b71e2fb93ecedd92c50cfa7caa7403397b358cb8
Rachana Devi <rachanadevi18@gmail.com>
2023-03-17 16:13:44 +0530

    Add primitive datatypes

---
+++ dist/primitive_dataypes.js
@@ -0,0 +1,9 @@
+"use strict";
+let sales = 123345;
+let course = 'Typescript';
+let is_published = true;
+let other_sales = 1234434;
+let other_course = 'Typescript course';
+let other_is_published = true;
+let anyVariable;
+//# sourceMappingURL=primitive_dataypes.js.map
---
+++ src/primitive_dataypes.ts
@@ -0,0 +1,11 @@
+// with annotation of type of variables works
+let sales: number = 123_345; // a big number can be stored with underscores
+let course: string = 'Typescript';
+let is_published: boolean = true;
+
+// without annotation of datatypes it also works
+let other_sales = 1234_434;
+let other_course = 'Typescript course'
+let other_is_published = true;
+
+let anyVariable; // this is of type any
