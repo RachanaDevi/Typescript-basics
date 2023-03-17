ad40855db4d156cce88e8497ee6dd44bb7214ba0
Rachana Devi <rachanadevi18@gmail.com>
2023-03-17 16:36:17 +0530

    Update tsconfig to have compiler checks for unused arguments and have a proper return type and have optional, default values for arguments

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,29 @@
 // at the end is the return type of the function and properly annotate our functions
 // if you don't return anything it will take it as void
-function calculateTax(income: number): number{
-    return 0; 
+
+function calculateTax(income: number, taxYear: number): number{
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
 }
+
+// ? means optional
+function calculateTaxOptional(income: number, taxYear?: number): number{
+    if ((taxYear || 2022) < 2022) { // one way to solve error if taxYear is not defined
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+
+// adding default value for taxYear
+function calculateTaxWithDefaultTaxYear(income: number, taxYear = 2023): number {
+    if (taxYear < 2022) { // one way to solve error if taxYear is not defined
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+
+
+// you can exactly pass 2 arguments unlike javascript
+calculateTax(10_000, 2022);
