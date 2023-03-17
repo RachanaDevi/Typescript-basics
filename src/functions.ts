2b0b6f546c69cdb3d2b8111acc567b90a645d3c9
Rachana Devi <rachanadevi18@gmail.com>
2023-03-17 16:29:29 +0530

    Add typescript file for functions

---
+++ src/functions.ts
@@ -0,0 +1,5 @@
+// at the end is the return type of the function and properly annotate our functions
+// if you don't return anything it will take it as void
+function calculateTax(income: number): number{
+    return 0; 
+}
